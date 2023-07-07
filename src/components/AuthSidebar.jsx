54a6b6b01b15fedc9b63f51ccdbe57d25c466492
queen <queenogbu111@gmail.com>
2023-07-07 07:32:42 +0100

    first commit

---
+++ src/components/AuthSidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AuthSidebar = () => {
+    return (
+        <div className="col-3 login-sidebar ps-5 pd-3">
+            <div className="card">
+                <nav className="nav flex-column">
+                    <a className="nav-link" href="/login">Login</a>
+                    <a className="nav-link" href="/register">Register</a>
+                    <a className="nav-link" href="#">Forgotten Password</a>
+                    <a className="nav-link" href="#">My Account</a>
+            
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default AuthSidebar;
---
+++ src/components/Header.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import enGbFlag from '../assets/images/language/en-gb/en-gb.png';
+
+const Header = () => {
+  return (
+    <header>
+        {/* <div className="container-fluid px-5 py-2 nav-box">
+            <div className="row welcome-top">
+                <div className="col-8 fs-6">The Access To Endless Educational Content</div>
+            </div>
+        </div> */}
+
+        
+        <div class="row my-4">
+            <div class="col-2 py-2">
+                <img src="https://th.bing.com/th/id/OIP.IPUDTmz_43gYqMmWUGo2xgHaC1?w=335&h=133&c=7&r=0&o=5&pid=1.7" alt="EduTech" class="logo" height={40}/>
+            </div>
+            <div class="col-10">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div
+                        class="container-fluid">
+                        {/* <!-- <a class="navbar-brand" href="#">Navbar</a> --> */}
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">BOOKS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">MAGAZINES</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">AUDIOBOOKS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">VOCATIONAL SKILLS</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        MORE
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li>
+                                            <a class="dropdown-item" href="#">Action</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                        </li>
+                                        <li><hr class="dropdown-divider" /></li>
+                                        <li>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <div class="header-links text-end">
+                                <span>
+                                    <i class="fa fa-lock"></i> <a href="/login">Login</a> | <a href="/register">Register</a>
+                                </span>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div className="row my-4">
+            <div className="col-2-5">
+                <button className="header-btn">
+                    <i className="fa fa-bars"></i>
+                    <div className="d-inline-block px-3">PROGRAMS</div>
+                    <i className="fa fa-angle-down"></i>
+                </button>
+            </div>
+            <div className="col-5-5">
+                <div className="input-group">
+                    <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Search for courses</button>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <a className="dropdown-item" href="#">Action before</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Another action before</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                        </li>
+                        <li><hr className="dropdown-divider" /></li>
+                        <li>
+                            <a className="dropdown-item" href="#">Separated link</a>
+                        </li>
+                    </ul>
+                    <input type="text" className="form-control" aria-label="Text input with 2 dropdown buttons" placeholder="Search" />
+                    <button className="px-5 header-btn">
+                        <i className="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+            
+    </header>
+  );
+};
+
+export default Header;
